93b6f0984d3c6bf041799c590370f2d2875a9ae6
Raidsoul <Thunderock01@gmail.com>
2019-01-15 18:37:49 +0700

    damn jerd

--- NormalClassify.py
+++ NormalClassify.py
@@ -7,6 +7,15 @@ import subprocess
 from imutils import contours
 from imutils.perspective import four_point_transform
 
+# Damn Shitty
+isPeriod = bool(False)
+isEatBreakfast = False
+isEatLunch = False
+isEatDinner = False
+isEatBedTime = False
+isRoutine = False
+periodHour = 0
+
 # str0 = "ก่อนอาหาร"
 # str1 = "หลังอาหาร"
 # str2 = "เช้า"
@@ -42,21 +51,63 @@ def Spell_checker(name):
         if(line.find(strB1) > 0):
             print ('ก่อนอาหาร')
             if(line.find(str2) >0):
+                isEatBreakfast = True
                 print('เช้า')
             if(line.find(str3) >0):
                 print('กลางวัน')
+                isEatLunch = True
             if(line.find(str4) >0):
                 print('เย็น')
+                isEatDinner = True
         if(line.find(strA1) > 0 or line.find(strA2) > 0):
             print ('หลังอาหาร')
             if(line.find(str2) >0):
                 print('เช้า')
+                isEatBreakfast = True
+                print(isEatBreakfast)
             if(line.find(str3) >0):
                 print('กลางวัน')
+                isEatLunch = True
             if(line.find(str4) >0):
                 print('เย็น')
+                isEatDinner = True
+            print(isEatBreakfast)
         line = f.readline()
-    
+
+def JSON_Creator(_isPeriod, _isEatBreakfast, _isEatLunch, _isEatDinner, _isEatBedTime, _isRoutine, _periodHour) :
+    temp = open("temp.txt", "w")
+    temp.write("{")
+    if _isPeriod == False :
+        temp.write ("\"" + "isPeriod" + "\"" + " : " + "false ,")
+
+        temp.write ("\"" + "Data" + "\"" + " : " + "{")
+
+        if _isEatBreakfast :
+            temp.write ("\"" + "isEatBreakfast" + "\""+ " : " + "true ,")
+        else : temp.write ("\"" + "isEatBreakfast" + "\""+ " : " + "false ,")
+        if _isEatLunch :
+            temp.write ("\"" + "isEatLunch" + "\""+ " : " + "true ,")
+        else : temp.write ("\"" + "isEatLunch" + "\""+ " : " + "fasle ,")
+        if _isEatDinner :
+            temp.write ("\"" + "isEatDinner" + "\""+ " : " + "true ,")
+        else : temp.write ("\"" + "isEatDinner" + "\""+ " : " + "fasle ,")
+        if _isEatBedTime :
+            temp.write ("\"" + "isEatBedTIme" + "\""+ " : " + "true")
+        else : temp.write ("\"" + "isEatBedTIme" + "\""+ " : " + "false")
+
+        temp.write("}")
+        
+    if _isPeriod == True : 
+        temp.write("\"" + "isPeriod" + "\"" + " : " + "ture ,")
+        
+        temp.write ("\"" + "Data" + "\"" + " : " + "{")
+        if isRoutine :
+            temp.write("\"" + "isRoutine" + "\"" + " : " + "true,")
+        else : temp.write("\"" + "isRoutine" + "\"" + " : " + "false,")
+        temp.write("\"" + "PeriodHour" + "\"" + " : " + str(periodHour))
+        temp.write("}")
+
+    temp.write("}")
 
 def main(argv) :
     image = cv2.imread(argv[0]) 
@@ -67,15 +118,10 @@ def main(argv) :
     edged = cv2.Canny(blurred, 50, 200, 255)
     kernel = np.ones((2,8),np.uint8)
     dilation = cv2.dilate(edged,kernel,iterations = 1)
-    cv2.imshow('dilation' , dilation)
-    # kernel2 = np.ones((5,1),np.uint8)
-    # erosion = cv2.erode(dilation,kernel2,iterations = 1)
-    # kernel3 = np.ones((7,1),np.uint8)
-    # dilation2 = cv2.dilate(erosion,kernel3,iterations = 1)
     contourmask,contours,hierarchy = cv2.findContours(dilation,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)
     fname = argv[0].split(".")[0]
-    
+     
     with open(fname+".txt","w") as f:
         for cnt in contours[1:] :
             x, y, w, h = cv2.boundingRect(cnt)
@@ -85,8 +131,11 @@ def main(argv) :
                 cv2.imwrite( str(w*h) + ".png" , roi)
                 f.write(text_from_image_file( str(w*h) + ".png",'tha'))
                 os.remove( str(w*h) + ".png")
-    cv2.imshow('img' , image)
-    cv2.waitKey(0)
-    Spell_checker(fname) 
+
+    # Doing Some JSON
+
+    Spell_checker(fname)
+    print(isEatBreakfast)
+    JSON_Creator(isPeriod, isEatBreakfast, isEatLunch, isEatDinner, isEatBedTime, isRoutine, periodHour)
     
 main(sys.argv[1:])
